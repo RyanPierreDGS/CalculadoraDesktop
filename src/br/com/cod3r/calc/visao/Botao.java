0c4a2ef8f2954507b09e93a57821e4df1e6cd9fa
Ryan Pierre <ryanpierre2014@gmail.com>
2025-05-15 20:57:16 -0400

    Resultado-parcial final

--- src/br/com/cod3r/calc/visao/Botao.java
+++ src/br/com/cod3r/calc/visao/Botao.java
@@ -1,8 +1,22 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton {
 
+	public Botao(String text, Color cor) {
+		
+		setText(text);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("courier", Font.PLAIN, 25)); // definindo estilo da fonte e tamnho
+		setForeground(Color.white); // cor da fonte
+		setBorder(BorderFactory.createLineBorder(Color.black)); // alterando cor da borda
+	}
+	
 }
--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -6,20 +6,28 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import br.com.cod3r.calc.modelo.Memoria;
+import br.com.cod3r.calc.modelo.MemoriaObservador;
+
 @SuppressWarnings("serial")
-public class Display extends JPanel {
+public class Display extends JPanel implements MemoriaObservador {
 
 	private final JLabel label;
 	
 	public Display() {
+		Memoria.getinstancia().adicionarObservador(this);
 		
 		setBackground(new Color(46, 49, 50));
-		label = new JLabel("1234,56");
+		label = new JLabel(Memoria.getinstancia().getTextoAtual());
 		label.setForeground(Color.white);
 		label.setFont(new Font("courier", Font.PLAIN, 30));
 		
 		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
 		
 		add(label);
+	}
+	
+	public void valorAlterado(String novoValor) {
+		label.setText(novoValor);
 	}
 }
